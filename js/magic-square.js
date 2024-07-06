2d919177797019605b74f8add7c595c56130657b
AomiVel <kaitoyoutube@icloud.com>
2024-07-06 19:49:27 +0900

    ボタン一つでグリッド生成から数字埋めまでできるように変更

--- js/magic-square.js
+++ js/magic-square.js
@@ -1,62 +1,70 @@
-function createMagicSquare() {
+async function createMagicSquare() {
     let size = Number(document.getElementById("size").value);
+    if (size < 1) {
+        alert("1以上の数値を入力してください");
+        return;
+    }
     let magicSquare = document.getElementById("magic-square");
 
-    // setGrid(size, magicSquare);
-
-    if (size % 2 === 0) {
-        createEvenSquare(size, magicSquare);
-    }
-    else {
-        createOddSquare(size, magicSquare);
-    }
-}
-
-function setGrid() {
-    let size = Number(document.getElementById("size").value);
-    let elem = document.getElementById("magic-square");
-
-    let removingChildren = true;
-    function removeChildrenLoop() {
-        if (elem.firstChild) {
-            elem.removeChild(elem.firstChild);
-            requestAnimationFrame(removeChildrenLoop);
+    setGrid(size, magicSquare).then(() => {
+        if (size % 2 === 0) {
+            createEvenSquare(size, magicSquare);
         }
         else {
-            removingChildren = false;
+            createOddSquare(size, magicSquare);
         }
-    }
-    requestAnimationFrame(removeChildrenLoop);
+    });
+}
 
-    let y = 0;
-    function loopY() {
-        if (removingChildren) {
-            return requestAnimationFrame(loopY);
+function setGrid(size, elem) {
+    return new Promise((resolve) => {
+        let removingChildren = true;
+        function removeChildrenLoop() {
+            if (elem.firstChild) {
+                elem.removeChild(elem.firstChild);
+                requestAnimationFrame(removeChildrenLoop);
+            }
+            else {
+                removingChildren = false;
+            }
         }
-        let row = document.createElement("div");
-        row.classList.add("row");
-        elem.appendChild(row);
+        requestAnimationFrame(removeChildrenLoop);
 
-        let x = 0;
-        function loopX() {
-            let col = document.createElement("div");
-            col.classList.add("col");
-            row.appendChild(col);
+        let y = 0;
+        function loopY() {
+            if (removingChildren) {
+                return requestAnimationFrame(loopY);
+            }
+            let row = document.createElement("div");
+            row.classList.add("row");
+            row.classList.add(y.toString())
+            elem.appendChild(row);
 
-            x ++;
-            if (x < size) {
-                requestAnimationFrame(loopX);
+            let x = 0;
+            function loopX() {
+                let col = document.createElement("div");
+                col.classList.add("col");
+                row.appendChild(col);
+
+                x ++;
+                if (x < size) {
+                    requestAnimationFrame(loopX);
+                    return
+                }
+                if (row.classList.contains(size - 1)) {
+                    resolve();
+                }
+            }
+
+            requestAnimationFrame(loopX);
+            y ++;
+            if (y < size) {
+                requestAnimationFrame(loopY);
             }
         }
 
-        requestAnimationFrame(loopX);
-        y ++;
-        if (y < size) {
-            requestAnimationFrame(loopY);
-        }
-    }
-
-    requestAnimationFrame(loopY);
+        requestAnimationFrame(loopY);
+    });
 }
 
 // 奇数
